fc64df78e9a2fdaec58eca7ef2f73c21bc35a2c6
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-24 17:40:14 +0000

    Update: '2-puppet_custom_http_response_header.pp' modified to restart nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,3 +4,8 @@ file {'/etc/nginx/sites-available/default':
   ensure  => present,
   content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\tadd_header X-Served-By \$hostname;\n\trewrite ^/redirect_me(.*)$ http://www.youtube.com/ permanent;\n}\n",
 }
+
+exec {'restart nginx':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
+}
