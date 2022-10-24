8a652fe5866be8097466ebb8617ab21a8aa8afe5
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-24 18:14:11 +0000

    Update: '2-puppet_custom_http_response_header.pp' modified

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,12 @@
 # Configures nginx server by adding custom headers
 
 exec {'install nginx':
-  command => 'apt-get update;sudo apt-get -y install nginx',
+  command => 'apt-get -y update;sudo apt-get -y install nginx',
   path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
-}
-
-exec {'change permissions':
-  command => 'sudo chown $USER:$USER /etc/nginx/sites-available/default',
-  path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
-}
-
-file {'/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\tadd_header X-Served-By \$hostname;\n\trewrite ^/redirect_me(.*)$ http://www.youtube.com/ permanent;\n}\n",
 }
 
 exec {'restore permissions':
-  command => 'sudo chown root:root /etc/nginx/sites-available/default',
+  command => 'sudo sed -i "/listen 80 default_server;/a add_header     X-Served-By \$HOSTNAME;" /etc/nginx/sites-available/default;',
   path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
 }
 
