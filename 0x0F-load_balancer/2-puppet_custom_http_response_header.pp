831c7c1fbf9740fc332ffa9a1e15a3bc22c38381
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-24 19:07:23 +0000

    Update: '2-puppet_custom_http_response_header.pp' modified

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,9 @@
-# Configures nginx server by adding custom headers
+# configures a new server with nginx and custom headers
 
-exec {'install nginx':
-  command => 'apt-get -y update;sudo apt-get -y install nginx',
-  path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
-}
-
-exec {'restore permissions':
-  command  => 'sudo sed -iE "/listen 80 default_server;/a      add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default;',
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -iE "s/listen 80 default_server;/listen 80 default_server; \n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
   provider => shell,
-}
-
-exec {'restart nginx':
-  command => 'sudo service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
 }
