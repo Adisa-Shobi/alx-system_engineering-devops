a44f67a81ca09e3ba46cd09008678667a01b17e1
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-24 17:25:33 +0000

    Create: '2-puppet_custom_http_response_header.pp' uses puppet to configure nginx with custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,6 @@
+# Configures nginx server by adding custom headers
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\tadd_header X-Served-By \$hostname;\n\trewrite ^/redirect_me(.*)$ http://www.youtube.com/ permanent;\n}\n",
+}
