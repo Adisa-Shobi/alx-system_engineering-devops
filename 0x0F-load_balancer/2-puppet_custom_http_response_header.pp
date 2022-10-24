9febc1c3cb8c4fc410a9e54fde4588b1899f1d26
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-24 17:56:00 +0000

    Update: '2-puppet_custom_http_response_header.pp' works without sudo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,21 @@
 # Configures nginx server by adding custom headers
 
+exec {'change permissions':
+  command => 'sudo chown $USER:$USER /etc/nginx/sites-available/default',
+  path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
+}
+
 file {'/etc/nginx/sites-available/default':
   ensure  => present,
   content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\troot /var/www/html;\n\tindex index.html index.htm index.nginx-debian.html;\n\tserver_name _;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\tadd_header X-Served-By \$hostname;\n\trewrite ^/redirect_me(.*)$ http://www.youtube.com/ permanent;\n}\n",
 }
 
+exec {'restore permissions':
+  command => 'sudo chown root:root /etc/nginx/sites-available/default',
+  path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
+}
+
 exec {'restart nginx':
-  command => 'service nginx restart',
+  command => 'sudo service nginx restart',
   path    => '/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/bin:/sbin'
 }
