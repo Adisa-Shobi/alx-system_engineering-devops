c2a884f37ea1ca67d1f80726be70960ebea99127
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-18 19:34:19 +0000

    Update: '1-export_to_CSV.py' data changed from name to username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,23 +12,15 @@ if __name__ == "__main__":
         api_endpoint = "https://jsonplaceholder.typicode.com"
         user_id = sys.argv[1]
         user_data = requests.get(api_endpoint + "/users/" + user_id).json()
-        employee_name = user_data.get('name')
+        username = user_data.get('username')
         todo_data = \
             requests.get(api_endpoint + "/users/" + user_id + "/todos").\
             json()
-        total_num_of_tasks = len(todo_data)
-        completed_tasks = [todo for todo in todo_data
-                           if todo['completed'] is True]
-        num_of_done_tasks = len(completed_tasks)
-        line_one = "Employee {} is done with tasks({}/{}):".format(
-            employee_name,
-            num_of_done_tasks,
-            total_num_of_tasks)
         with open("{}.csv".format(user_id), 'w') as csv_file:
             for task in todo_data:
                 csv_file.write('"{}","{}","{}","{}"\n'.format(
                     user_id,
-                    employee_name,
+                    username,
                     task.get('completed'),
                     task.get('title')
                 ))
