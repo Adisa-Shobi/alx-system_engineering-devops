1475910d04ae1e8fcde67d08cc2a4c6f645a732f
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-18 18:34:50 +0000

    Update: '0-gather_data_from_an_API.py' modified

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,22 +8,21 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        api_endpoint = "https://jsonplaceholder.typicode.com/"
+        api_endpoint = "https://jsonplaceholder.typicode.com"
         user_id = sys.argv[1]
-        with requests.get(api_endpoint + "users/" + user_id) as response:
-            json_data = response.json()
-            employee_name = json_data.get('name')
-        with requests.get(api_endpoint + "users/" + user_id + "/todos")\
-             as response:
-            json_data = response.json()
-            total_num_of_tasks = len(json_data)
-            completed_tasks = [todo for todo in json_data
-                               if todo['completed'] is True]
-            num_of_done_tasks = len(completed_tasks)
-            line_one = "Employee {} is done with tasks({}/{}):".format(
-                employee_name,
-                num_of_done_tasks,
-                total_num_of_tasks)
+        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
+        employee_name = user_data.get('name')
+        todo_data = \
+            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
+            json()
+        total_num_of_tasks = len(todo_data)
+        completed_tasks = [todo for todo in todo_data
+                           if todo['completed'] is True]
+        num_of_done_tasks = len(completed_tasks)
+        line_one = "Employee {} is done with tasks({}/{}):".format(
+            employee_name,
+            num_of_done_tasks,
+            total_num_of_tasks)
         print(line_one)
         for task in completed_tasks:
             print("\t {}".format(task["title"]))
