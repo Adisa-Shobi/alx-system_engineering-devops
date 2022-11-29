c1ba095698f113cfe6a7c08bbd6dd8b0a3cd43d6
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-29 18:32:19 +0000

    Update: '0-subs.py, 1-top_ten.py' with block removed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,11 +18,11 @@ def number_of_subscribers(subreddit):
     endpoint = 'https://www.reddit.com'
     headers = {'user-agent': 'Mozilla/5.0 \
 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
-    with requests.get('{}/r/{}/about.json'.format(
+    info = requests.get('{}/r/{}/about.json'.format(
             endpoint,
-            subreddit), headers=headers, allow_redirects=False) as info:
-        if info.status_code == 200:
-            json_info = info.json()
-            return(json_info.get('data').get('subscribers'))
-        else:
-            return(0)
+            subreddit), headers=headers, allow_redirects=False)
+    if info.status_code == 200:
+        json_info = info.json()
+        return(json_info.get('data').get('subscribers'))
+    else:
+        return(0)
