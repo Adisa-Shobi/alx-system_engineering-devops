7fd83a1269ef3bb2c86d7321d29d44f2eec5fc92
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-29 11:34:23 +0000

    Update: '0-subs.py' argument checks added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,13 +13,14 @@ def number_of_subscribers(subreddit):
     Return:
         0 - if invalid subreddit is given
     '''
+    if subreddit is None or not isinstance(subreddit, str):
+        return(0)
     endpoint = 'https://www.reddit.com'
-    sub_reddit = sys.argv[1]
     headers = {'user-agent': 'Mozilla/5.0 \
 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
     with requests.get('{}/r/{}/about.json'.format(
             endpoint,
-            sub_reddit), headers=headers, allow_redirects=False) as info:
+            subreddit), headers=headers, allow_redirects=False) as info:
         if info.status_code == 200:
             json_info = info.json()
             return(json_info.get('data').get('subscribers'))
