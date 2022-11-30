817aadc23f9140f24ee31eb260bed0dfef79857c
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-30 13:46:36 +0000

    Update: '0-strace_is_your_friend.pp' puppet script for typo fix in wp-settings.php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Debugs apache2 server error
 
-exec { 'mv /var/www/html/readme.html /var/www/html/index.html':
+exec { 'sed -i s/phpp/php/g /var/www/html/wp-settings.php':
   path => '/usr/bin:/usr/sbin:/bin'
 }
